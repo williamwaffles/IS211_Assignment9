86fa1f4db9fe516c8a2a05bb68f667a00c5b72c8
williamwaffles <41193361+williamarnaiz@users.noreply.github.com>
2021-10-30 12:56:57 -0400

    Updated Code!

--- football_stats.py
+++ football_stats.py
@@ -1,2 +1,13 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import ssl
+
+def main():
+    url = 'https://www.cbssports.com/nfl/stats/player/passing/nfl/regular/qualifiers/?sortcol=td&sortdir=descending'
+    response = urllib.request.urlopen(url)
+    soup = BeautifulSoup(response.read(), features='lxml')
+    player_info = soup.find_all('tr')[1:21]
+    top_twenty = []
+
 if __name__ == "__main__":
-    pass
+    main()
--- nfl_spreads.py
+++ nfl_spreads.py
@@ -1,2 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+import parser
+
+URL = "http://www.footballlocks.com/nfl_point_spreads.shtml"
+
+def remove_at(name):
+    return name.replace('At', '')
+
 if __name__ == "__main__":
-    pass
+
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.text, features = 'xml')
+    tables = soup.find_all('table', cols = '4', width = '580')
+    rows = tables[0].find_all('tr')
+
+    header = True
+    for row in rows:
+        cells = row.find_all('td')
+        if header:
+            print(f'{cells[1].text:<15} | {cells[3].text:<15} | {cells[2].text}')
+            header = False
+            continue
+
+        favorite = remove_at(cells[1].text)
+        spread = float(cells[2].text)
+        underdog = remove_at(cells[3].text)
+
+        print(f"{favorite:<15} | {underdog:<15} | {spread:.1f}")
