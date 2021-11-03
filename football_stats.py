c65c55180a7a9ed2f692fd2d5a66e34a1450a913
williamwaffles <41193361+williamarnaiz@users.noreply.github.com>
2021-11-03 10:21:06 -0400

    Updated code!

--- football_stats.py
+++ football_stats.py
@@ -1,13 +1,32 @@
 from bs4 import BeautifulSoup
 import urllib.request
-import ssl
+import pandas as pd
 
-def main():
-    url = 'https://www.cbssports.com/nfl/stats/player/passing/nfl/regular/qualifiers/?sortcol=td&sortdir=descending'
-    response = urllib.request.urlopen(url)
-    soup = BeautifulSoup(response.read(), features='lxml')
-    player_info = soup.find_all('tr')[1:21]
-    top_twenty = []
+URL = "https://www.cbssports.com/nfl/stats/player/passing/nfl/regular/qualifiers/?sortcol=td&sortdir=descending"
 
 if __name__ == "__main__":
-    main()
+
+    response = urllib.request.urlopen(URL)
+    soup = BeautifulSoup(response.read(), 'html.parser')
+    tables = soup.find_all('tr')[1:21]
+    top_twenty = []
+
+    for row in tables:
+
+        stats = {}
+        player_name = row.select('a')[1].contents[0]
+        player_position = row.select('span')[2].contents[0].strip()
+        player_team = row.select('span')[3].contents[0].strip()
+        passing_yards = row.select('td')[5].contents[0].strip()
+        passing_touchdowns = row.select('td')[8].contents[0].strip()
+        stats['Name'] = player_name
+        stats['Position'] = player_position
+        stats['Team'] = player_team
+        stats['Passing Yards'] = passing_yards
+        stats['Passing Touchdowns'] = passing_touchdowns
+        top_twenty.append(stats)
+
+    tables = pd.DataFrame(top_twenty)
+    tables.index += 1
+    print('Top 20 Players: ')
+    print(tables)
--- nfl_spreads.py
+++ nfl_spreads.py
@@ -1,16 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
-import parser
 
 URL = "http://www.footballlocks.com/nfl_point_spreads.shtml"
 
 def remove_at(name):
-    return name.replace('At', '')
+    return name.replace('At ', '')
 
 if __name__ == "__main__":
 
     response = requests.get(URL)
-    soup = BeautifulSoup(response.text, features = 'xml')
+    soup = BeautifulSoup(response.text, 'html.parser')
     tables = soup.find_all('table', cols = '4', width = '580')
     rows = tables[0].find_all('tr')
 
@@ -23,7 +22,7 @@ if __name__ == "__main__":
             continue
 
         favorite = remove_at(cells[1].text)
-        spread = float(cells[2].text)
         underdog = remove_at(cells[3].text)
+        spread = float(cells[2].text)
 
         print(f"{favorite:<15} | {underdog:<15} | {spread:.1f}")
