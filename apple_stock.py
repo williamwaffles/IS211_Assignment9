e03ba7290418827b41b6650f713f686a071bf977
williamwaffles <41193361+williamarnaiz@users.noreply.github.com>
2021-11-07 10:26:39 -0500

    Update apple_stock.py
    Added some parse components - cannot figure out how to pull out the closing price

--- apple_stock.py
+++ apple_stock.py
@@ -1,2 +1,23 @@
-if __name__ == "__main__":
-    pass
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+
+
+URL = 'https://finance.yahoo.com/quote/AAPL/history?p=AAPL'
+page = requests.get(URL, headers = {'User-Agent' : 'Custom'})
+content = page.content
+soup = BeautifulSoup(content, 'html.parser', from_encoding="utf-8")
+
+data = soup.find_all('tr', attrs= 'BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)')
+print(data)
+
+
+#if __name__ == "__main__":
+
+for row in range(len(data)):
+    '''close_date = i.find_all('span')[0].text.strip()
+    close_value = i.find_all('span')[4].text.strip()'''
+    parse = data[row].find_all('span')
+    date = parse[0].text.strip()
+    open = parse[1].text.strip()
+
